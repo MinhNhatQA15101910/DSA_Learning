9cf6e566215abb1c72eef82855a9585d1f8cf848
Do Nhat <dmnhat.pt@gmail.com>
2024-11-05 00:45:30 +0700

    first commit

---
+++ BigO/FindNemo/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static readonly string[] nemo = ["nemo"];
+
+    public static void FindNemo(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == "nemo")
+            {
+                Console.WriteLine("Found NEMO!");
+            }
+        }
+    }
+
+
+    static void Main()
+    {
+        FindNemo(nemo);
+    }
+}
